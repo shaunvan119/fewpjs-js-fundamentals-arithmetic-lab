7601f77584c1d2e332aaab59cc68c16a2ef384f9
shaunvan119 <shaunvan@live.com.au>
2021-03-24 20:29:23 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
-// Write your code here
+
+
+let num1 = 12.4
+let num2 = 5
+
+let multiply = num1*num2
+
+console.log(multiply);
+
+
+
+let random = Math.floor(Math.random() > 0) + 3;
+
+console.log(random);
+
+
+let num3 = 12;
+let num4 = 8;
+let mod = num3 % num4;
+
+console.log(mod);
+
+
+let max = Math.max(20);
+
+console.log(max);
+
+
+
